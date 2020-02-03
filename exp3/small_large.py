a16cbdad4582d0fc217ccdafb449745b6d064698
ItsMeVArun5 <52820886+ItsMeVArun5@users.noreply.github.com>
2020-02-03 17:18:34 +0530

    Delete small_large.py

--- exp3/small_large.py
+++
@@ -1,6 +0,0 @@
-# Write a Python program to get the smallest and largest number from a list.
-
-numbers = [123,4,234,13,40,11]
-
-print("largest number in a list is: " + str(max(numbers)))
-print("smallest number in a list is: " + str(min(numbers)))
