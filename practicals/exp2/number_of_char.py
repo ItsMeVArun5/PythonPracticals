d818c958ef9d789bdf4a871ec8d11dabd2773adb
ItsMeVArun5 <52820886+ItsMeVArun5@users.noreply.github.com>
2020-02-10 16:56:35 +0530

    without dictionaries

--- practicals/exp2/number_of_char.py
+++ practicals/exp2/number_of_char.py
@@ -1,12 +1,22 @@
 # W.A.P.P to count the number of characters(character frequency) in a string.
 
+count = 0
+element = []
+
 string = "google.com"
-freq = {}
 
 for i in string:
-	if i in freq:
-		freq[i] += 1
-	else:
-		freq[i] = 1
+    if i not in element:
+        element.append(i)
+    else:
+        continue
 
-print (freq)
+    for x in string:
+        if i == x:
+            count+=1
+
+    element.append(count)
+    count = 0
+        
+
+print (element)
