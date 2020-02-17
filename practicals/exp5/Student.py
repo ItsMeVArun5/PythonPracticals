021072311d970a1f79d6e9131bda1112253dde87
ItsMeVArun5 <varunmewada1@gmail.com>
2020-02-17 23:18:36 +0530

    Student grade system

--- practicals/exp5/Student.py
+++ practicals/exp5/Student.py
@@ -1,19 +1,22 @@
 class Student:
         def __init__(self, ):
-            self.giveDetails()
+                self.giveDetails()
 
         def giveDetails(self,):
-            name = input("Enter name: ")
-            self.name = name
-            rollno = int(input("Enter rollno: "))
-            self.rollno = rollno
-            dept = input("Enter departement name: ")
-            self.dept = dept
-            pointer = int(input("Enter pointer: "))
-            self.pointer = pointer
+
+                name = input("Enter name: ")
+                self.name = name
+                age = input("Enter Age: ")
+                self.age = age
+                rollno = int(input("Enter rollno: "))
+                self.rollno = rollno
+                dept = input("Enter departement name: ")
+                self.dept = dept
+                pointer = int(input("Enter pointer: "))
+                self.pointer = pointer
 
         def Result(self, ):
-            student_class = {
+                student_class = {
                 '10': 'outstanding',
                 '9': 'Excellent',
                 '8': 'Very Good',
@@ -26,23 +29,34 @@ class Student:
                 '1': 'Fail',
                 '0': 'Fail',
             }
-            print ("Grade(pointer) \t \t Class")
-            if self.pointer <= 10 and self.pointer >9:
-                print (self.pointer, "\t \t",   student_class['10'] )
-            elif self.pointer <= 9 and self.pointer > 8:
-                    print (self.pointer, "\t \t",   student_class['9'] )
-            elif self.pointer <= 8 and self.pointer > 7:
-                    print (self.pointer, "\t \t",   student_class['8'] )
-            elif self.pointer <= 7 and self.pointer > 6:
-                    print (self.pointer, "\t \t",   student_class['7'] )
-            elif self.pointer <= 6 and self.pointer > 5:
-                    print (self.pointer, "\t \t",   student_class['6'] )
-            elif self.pointer <= 5 and self.pointer > 4:
-                    print (self.pointer, "\t \t",   student_class['5'] )
-            elif self.pointer == 4:
-                    print (self.pointer, "\t \t",   student_class['4'] )
-            else:
-                print (self.pointer, "\t \t",   student_class['3'])
+                self.getDetails()
+                print ("----------------------Class Obtained-----------------------")
+                print ("Grade(pointer) \t  Class")
+                if self.pointer <= 10 and self.pointer >9:
+                        print (self.pointer, "\t \t",   student_class['10'] )
+                elif self.pointer <= 9 and self.pointer > 8:
+                        print (self.pointer, "\t \t",   student_class['9'] )
+                elif self.pointer <= 8 and self.pointer > 7:
+                        print (self.pointer, "\t \t",   student_class['8'] )
+                elif self.pointer <= 7 and self.pointer > 6:
+                        print (self.pointer, "\t \t",   student_class['7'] )
+                elif self.pointer <= 6 and self.pointer > 5:
+                        print (self.pointer, "\t \t",   student_class['6'] )
+                elif self.pointer <= 5 and self.pointer > 4:
+                        print (self.pointer, "\t \t",   student_class['5'] )
+                elif self.pointer == 4:
+                        print (self.pointer, "\t \t",   student_class['4'] )
+                else:
+                    print (self.pointer, "\t \t",   student_class['3'])
+
+        def getDetails(self, ):
+                print ("----------------Student details--------------------- ")
+                print ("Name: ", self.name)
+                print ("Age: ", self.age)
+                print ("Rollno: ", self.rollno)
+                print ("Departement Name: ", self.dept)
+                print ("Pointer: ", self.pointer)
+                print ()
 
 
 s1 = Student()
